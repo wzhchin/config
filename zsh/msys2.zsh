113dd45c54663f8739d9935dd1b3adeb2535ef48
wangzh <wzhchin@outlook.com>
2026-01-20 09:21:21 +0800

    zsh: update

--- zsh/msys2.zsh
+++ zsh/msys2.zsh
@@ -3,5 +3,7 @@ alias jdk8='export JAVA_HOME=/d/Tools/jdks/jdk-8/; export PATH=$JAVA_HOME/bin:$P
 alias jdk17='export JAVA_HOME=/d/Tools/jdks/jdk-17; export PATH=$JAVA_HOME/bin:$PATH'
 alias jdk21='export JAVA_HOME=/d/Tools/jdks/jdk-21; export PATH=$JAVA_HOME/bin:$PATH'
 
+export PATH=/d/tools/nodejs:$PATH
+
 export PATH=/d/Tools/cmd:$PATH
 export PATH=/ucrt64/bin/:$PATH
