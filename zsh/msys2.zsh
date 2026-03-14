8c99e6d8a4f4ba89f69a277a07053ab97a16b0b0
wangzh <wzhchin@outlook.com>
2026-03-14 11:36:27 +0800

    zsh/msys2: update path for the input buffer

--- zsh/msys2.zsh
+++ zsh/msys2.zsh
@@ -1,4 +1,12 @@
 alias rg="rg --path-separator //"
+
+function chin-escape-backslashes {
+    LBUFFER=${LBUFFER//\\/\\\\}
+    RBUFFER=${RBUFFER//\\/\\\\}
+}
+zle -N chin-escape-backslashes
+bindkey '^\\' chin-escape-backslashes
+
 alias jdk8='export JAVA_HOME=/d/Tools/jdks/jdk-8/; export PATH=$JAVA_HOME/bin:$PATH'
 alias jdk17='export JAVA_HOME=/d/Tools/jdks/jdk-17; export PATH=$JAVA_HOME/bin:$PATH'
 alias jdk21='export JAVA_HOME=/d/Tools/jdks/jdk-21; export PATH=$JAVA_HOME/bin:$PATH'
