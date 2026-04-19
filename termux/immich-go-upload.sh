3968544372c29c391f5af670e643685cb2079454
wangzh <wzhchin@outlook.com>
2026-04-20 01:00:52 +0800

    termux: update init.sh

--- termux/immich-go-upload.sh
+++ termux/immich-go-upload.sh
@@ -2,4 +2,15 @@
 
 set -eu
 
-~/bin/immich-go upload from-folder --recursive --server "$IMMICH_URL" --api-key "$IMMICH_API_KEY" --pause-immich-jobs=FALSE /sdcard/DCIM /sdcard/Pictures/QQ /sdcard/Pictures/WeiXin /sdcard/Pictures/Screenshots
+UPLOAD_DIRS=(
+  /sdcard/DCIM 
+  /sdcard/Pictures/QQ 
+  /sdcard/Pictures/WeiXin 
+  /sdcard/Pictures/Screenshots
+  )
+
+immich-go upload from-folder --recursive \
+  --server "$IMMICH_URL" \
+  --api-key "$IMMICH_API_KEY" \
+  --pause-immich-jobs=FALSE \
+  "${UPLOAD_DIRS[@]}"
--- termux/init.sh
+++ termux/init.sh
@@ -1,7 +1,27 @@
 #!/usr/bin/env bash
 
-PKGS=(openssh vim zsh git)
+set -e
+
+PKGS=(openssh vim zsh git gdu uv)
+PIP_TOOLSS=(yt-dlp)
+IMMICH_GO_URL="https://github.com/simulot/immich-go/releases/download/v0.31.0/immich-go_Linux_arm64.tar.gz"
+
+BIN_DIR="$HOME/.local/bin"
+
+install_immich() {
+  local tdir=$(mktemp -d)
+  echo "tmpdir $tdir"
+  cd "$tdir"
+  wget "$IMMICH_GO_URL" -O "immich-go.tar.gz" > download.log
+  tar -xvf immich-go.tar.gz 
+  mv immich-go "$BIN_DIR"
+}
 
 pkg install "${PKGS[@]}"
 
+for p in "${PIP_TOOLS[@]}"; do
+  uv tool install "$p"
+done
 
+mkdir -p "$BIN_DIR"
+install_immich
